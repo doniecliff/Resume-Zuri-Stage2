37a4fae3943409b79e21bd751f0ddf76bda4b811
Daniel A-Williams <doniecliff143@gmail.com>
2021-08-21 03:51:17 +0100

    Resume Zuri-stage2
    complete Resume Task move from Stagw 2 => 3.. Join a Zuri team [21 Aug /21]

---
+++ README.md
@@ -0,0 +1,7 @@
+# Zuri internship
+## frontEnd Track
+
+### Stage 3 ~Task
+
+push resune with (me)js code to gitHub
+host on Netlify git@github.com:doniecliff/Resume-Zuri-Stage2.git
---
+++ js/name_out.js
@@ -0,0 +1,11 @@
+
+
+const authorsNames = ()=>{
+    names = "Daniel Williams";
+    return names;
+}
+
+
+document.getElementById("authorsName").innerHTML = authorsNames();
+
+// DOM manipulation
